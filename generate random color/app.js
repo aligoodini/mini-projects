2d57bed91c98b5406be353677a82d12f872c47e3
ali goodini <ilainidoog2001@gmail.com>
2023-09-06 21:59:23 +0430

    3-generate random color

---
+++ generate random color/app.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+
+setInterval(() => {
+    body.style.backgroundColor = `rgb(${Math.floor((Math.random()*255)+1)} ,
+   ${Math.floor((Math.random()*256))} ,
+   ${Math.floor((Math.random()*256))})` ;
+}, 2000);
