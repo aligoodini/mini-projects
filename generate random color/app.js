d2ceb4060c85904521abc3df83220064c3a0e424
ali goodini <ilainidoog2001@gmail.com>
2023-09-06 22:11:44 +0430

    genarate random color fixed

--- generate random color/app.js
+++ generate random color/app.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 
 setInterval(() => {
-    body.style.backgroundColor = `rgb(${Math.floor((Math.random()*255)+1)} ,
+    body.style.backgroundColor = `rgb(${Math.floor((Math.random()*256))} ,
    ${Math.floor((Math.random()*256))} ,
    ${Math.floor((Math.random()*256))})` ;
 }, 2000);
